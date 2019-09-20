2a14354234ec3a0178eb9055a5675f8fa0da173f
minielectron <prakash009Kp@gmail.com>
2019-09-20 12:06:33 +0530

    Basic structure is created

---
+++ app/src/main/java/com/androidcodeshop/obviousassigment/activities/DetailViewActivity.java
@@ -0,0 +1,15 @@
+package com.androidcodeshop.obviousassigment.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidcodeshop.obviousassigment.R;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
---
+++ app/src/main/java/com/androidcodeshop/obviousassigment/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package com.androidcodeshop.obviousassigment.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.androidcodeshop.obviousassigment.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.image_recycler_view)
+    RecyclerView imageRecyclerView;
+    @BindView(R.id.fab_share)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.fab_share)
+    public void onViewClicked() {
+    }
+}
